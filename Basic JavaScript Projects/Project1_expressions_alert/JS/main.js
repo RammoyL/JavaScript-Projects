26d9714e133cab9735c7e34223be6e3d5016ef37
Rammoy Lawrence <rammoylawrence@gmail.com>
2022-08-23 18:06:00 -0400

    Updated code
    Code styled in basic javascript

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+window.alert("Ain't much to look at right now.");
+
+var Teachers = "Subjects", Jones = "Biology", Fischer = "English",
+    Brooks = "Algebra", Tucker = "P.E.";
+document.write(Brooks);
+
+var x = 22 + 8; //assigning the variable x with a random value
+document.write(x); //executing code to have document to display the value of x
+
+function My_First_Function() {      //Defining a function and naming it
+    var str = "This is the button text!";       //Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str;  //Putting the value of the variable into HTML elementFromPoint
+                                                            //with the "Button_Text" id
+}
+
+var Sent1 = "This is the beginning of the string."; //Defining the first variable with a string value.
+var Sent2 = "This is the end of the string!"; //Defining the second variable with a string value.
+document.write("This is the beginning of the string." + " This is the end of the string.");
