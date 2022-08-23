c9b06bff300b421dd7b4c856ac85a3d075e3d033
Rammoy Lawrence <rammoylawrence@gmail.com>
2022-08-23 19:13:48 -0400

    Assignment 2
    Added more javascript code

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,13 @@
+window.alert("How'd finnd this? Go Away.");
+
+function My_First_Function() {      //Defining a function and naming it
+    var str = "See, nothing. No surprises at all!";       //Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML = str;  //Putting the value of the variable into HTML elementFromPoint
+    //with the "Button_Text" id
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += "a lot from this class!";
+    document.getElementById("Conctenate").innerHTML = sentence;
+}
