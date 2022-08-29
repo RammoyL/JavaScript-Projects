9c1db83e98a6168d58912728540506e5c290deeb
Rammoy Lawrence <rammoylawrence@gmail.com>
2022-08-29 00:26:20 -0400

    Arrays & Loops
    Submitted more JS code that utilizes arrays & loops

---
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -0,0 +1,56 @@
+function Call_Loop() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Loop").innerHTML = Digit;
+}
+
+function length_Function() {
+    let text = "What is the length of this string?";
+    let length = text.length;
+    document.getElementById("string_length").innerHTML = length;
+}
+
+var Instruments = ["Oboe", "Guitar", "Piano", "Drums", "Violin", "Saxophone", "Tuba"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+
+function Goku_techniques() {
+    var Goku_Moves = [];
+    Goku_Moves[0] = "Kamehameha";
+    Goku_Moves[1] = "Kaio Ken";
+    Goku_Moves[2] = "Spirit Bomb";
+    Goku_Moves[3] = "Dragon Fist";
+    Goku_Moves[4] = "Instant Teleportation";
+    document.getElementById("Goku").innerHTML = "The technique Goku will use is " + Goku_Moves[4] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = { type: "guitar", brand: "Fender", color: "blue" };
+    Musical_Instrument.color = "white";
+    Musical_Instrument.price = "$925";
+    Musical_Instrument.brand = "Fender";
+    document.getElementById("Constant").innerHTML = "The cost of this " + Musical_Instrument.type +
+        " is " + Musical_Instrument.price + ", and is manufacture by " + Musical_Instrument.brand;
+}
+function Cars() {
+    let car = {
+        make: "Aston Martin ",
+        model: " DBS Coupe",
+        year: "2021 ",
+        color: "Matte Black ",
+        description: function () {
+            return "The car is a " + this.year + this.color + this.make + this.model;
+        }
+    };
+    document.getElementById("Car_Object").innerHTML = car.description();
+}
